13fe9096e6a83b8333a3d01984d3f3058bd9f11d
sar2901 <sybilla.robertson@ucdconnect.ie>
2018-03-09 15:21:18 +0000

    Created test.py and test input file to test LEDCounter

---
+++ tests/test.py
@@ -0,0 +1,47 @@
+#COMP30670_Assignment3/tests/
+'''
+Created on Feb 28, 2018
+@author: Sybilla Robertson
+'''
+import sys
+sys.path.append('.')
+import unittest
+from LEDCounter.main import main, readInput, lightGrid, parseFile, on, off, switch, lightGridStatus
+
+
+class LEDCounterTest(unittest.TestCase):
+
+    def setUp(self):
+        self.inputTest = "testinput.txt"
+
+    def readInputTest(self):
+        readInput(self.inputTest)
+
+    def parseSwitchTest(self):
+        start, end, fun = parseFile('switch 0,0 through 4,4')
+        self.assertEquals(start, (322 , 558))
+        self.assertEquals(end, (977 , 958))
+
+    def parseOnOffTest(self):
+        start, end, fun = parseFile('turn on 0,0 through 4,4')
+        self.assertEquals(start, (0,0))
+        self.assertEquals(end, (4,4))
+
+        start, end, fun = parseFile('turn off 0,0 through 4,4')
+        self.assertEquals(start, (0,0))
+        self.assertEquals(end, (4,4))
+
+    def onOffSwitchTest(self):
+        self.assertTrue(on(True))
+        self.assertTrue(on(False))
+        self.assertFalse(off(True))
+        self.assertFalse(off(False))
+        self.assertFalse(switch(True))
+        self.assertTrue(switch(False))
+
+
+    def mainTest(self):
+        lightGridStatus(self.test_file)
+
+if __name__ == '__main__':
+    unittest.main()
